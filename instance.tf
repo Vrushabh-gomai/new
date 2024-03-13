937084e2841fde732c596ba59e39cd1813673d6c
1 <1@gmail.com>
2024-03-13 12:26:30 +0530

    nj

---
+++ instance.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    
+  }
+}
+
+provider "aws" {
+    region = "ap-southeast-1"
+    shared_config_files = [ "/root/.aws/config" ]
+    shared_credentials_files = [ "/root/.aws/credentials"]
+    profile = "Vrushabh"
+  
+}
+
+resource "aws_instance" "vrushabh" {
+    ami = "ami-03bb6d83c60fc5f7c"
+    instance_type = "t2.micro"
+    key_name = "tier"
+  
+}

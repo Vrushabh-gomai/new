6f90d5b3f8eac4bca22799b41b3e32d95280053c
1 <1@gmail.com>
2024-03-13 22:28:07 +0530

    lll

--- instance2.tf
+++ instance2.tf
@@ -8,13 +8,17 @@ provider "aws" {
     region = "ap-southeast-1"
     shared_config_files = [ "/root/.aws/config" ]
     shared_credentials_files = [ "/root/.aws/credentials"]
-    profile = "Vrushabh"
   
 }
 
 resource "aws_instance" "vrushabh" {
-    ami = "ami-09b9e25b6db1d130c"
+    ami = "ami-09b1e8fc6368b8a3a"
     instance_type = "t2.micro"
     key_name = "tier"
+    tags = {
+    Name        = "instance1"
+    Environment = "Production"
+    Owner       = "vrushabh"
+      }
   
 }

81d41fa5bede85cd1cf099873ea139f8ca02b504
1 <1@gmail.com>
2024-03-14 20:38:40 +0530

    ii

--- s3.tf
+++ s3.tf
@@ -1,3 +1,17 @@
+terraform {
+   required_providers {
+    
+   }
+ }
+
+ provider "aws" {
+     region = "ap-southeast-1"
+     shared_config_files = [ "/root/.aws/config" ]
+     shared_credentials_files = [ "/root/.aws/credentials"]
+  
+}
+
+
 resource "aws_s3_bucket" "new" {
   bucket = "my-bucket"
 

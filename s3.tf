4fb797f3bb6724459eb5e326812f7a8d2fcb6fd3
1 <1@gmail.com>
2024-03-14 21:09:43 +0530

    ss

--- s3.tf
+++ s3.tf
@@ -1,22 +1,22 @@
-terraform {
-   required_providers {
+# terraform {
+#    required_providers {
     
-   }
- }
+#    }
+#  }
 
- provider "aws" {
-     region = "ap-southeast-1"
-     shared_config_files = [ "/root/.aws/config" ]
-     shared_credentials_files = [ "/root/.aws/credentials"]
+#  provider "aws" {
+#      region = "ap-southeast-1"
+#      shared_config_files = [ "/root/.aws/config" ]
+#      shared_credentials_files = [ "/root/.aws/credentials"]
   
-}
+# }
 
 
-resource "aws_s3_bucket" "kit-kat" {
-  bucket = "kit-kat1"
+# resource "aws_s3_bucket" "kit-kat" {
+#   bucket = "kit-kat1"
 
-  tags = {
-    Name        = "kit"
-    Environment = "Dev"
-  }
-}
+#   tags = {
+#     Name        = "kit"
+#     Environment = "Dev"
+#   }
+# }

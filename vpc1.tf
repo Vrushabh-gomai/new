d6fe3bb2474f8561501e3f49a3ba1d7a5a3fb745
1 <1@gmail.com>
2024-03-14 21:14:21 +0530

    jj

--- vpc1.tf
+++ vpc1.tf
@@ -3,7 +3,12 @@ terraform {
     
    }
  }
-
+provider "aws" {
+      region = "ap-southeast-1"
+      shared_config_files = [ "/root/.aws/config" ]
+      shared_credentials_files = [ "/root/.aws/credentials"]
+  
+ }
 resource "aws_vpc" "vpc_v" {
   cidr_block       = "10.1.0.0/16"
   instance_tenancy = "default"

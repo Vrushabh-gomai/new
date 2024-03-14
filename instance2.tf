c389521a97fe035eec55e3a1fc52302fe9eea2d9
1 <1@gmail.com>
2024-03-14 19:54:32 +0530

    pp

--- instance2.tf
+++ instance2.tf
@@ -1,24 +1,24 @@
-terraform {
-  required_providers {
+# terraform {
+#   required_providers {
     
-  }
-}
+#   }
+# }
 
-provider "aws" {
-    region = "ap-southeast-1"
-    shared_config_files = [ "/root/.aws/config" ]
-    shared_credentials_files = [ "/root/.aws/credentials"]
+# provider "aws" {
+#     region = "ap-southeast-1"
+#     shared_config_files = [ "/root/.aws/config" ]
+#     shared_credentials_files = [ "/root/.aws/credentials"]
   
-}
+# }
 
-resource "aws_instance" "vrushabh" {
-    ami = "ami-09b1e8fc6368b8a3a"
-    instance_type = "t2.micro"
-    key_name = "tier"
-    tags = {
-    Name        = "instance1"
-    Environment = "Production"
-    Owner       = "vrushabh"
-      }
+# resource "aws_instance" "vrushabh" {
+#     ami = "ami-09b1e8fc6368b8a3a"
+#     instance_type = "t2.micro"
+#     key_name = "tier"
+#     tags = {
+#     Name        = "instance1"
+#     Environment = "Production"
+#     Owner       = "vrushabh"
+#       }
   
-}
+# }

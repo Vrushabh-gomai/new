27bb0306a5f5daff855814ade9d153730ddbba9e
1 <1@gmail.com>
2024-03-15 20:36:08 +0530

    yy

--- vpc1.tf
+++ vpc1.tf
@@ -1,19 +1,19 @@
-terraform {
-   required_providers {
+# terraform {
+#    required_providers {
     
-   }
- }
-provider "aws" {
-      region = "ap-southeast-1"
-      shared_config_files = [ "/root/.aws/config" ]
-      shared_credentials_files = [ "/root/.aws/credentials"]
+#    }
+#  }
+# provider "aws" {
+#       region = "ap-southeast-1"
+#       shared_config_files = [ "/root/.aws/config" ]
+#       shared_credentials_files = [ "/root/.aws/credentials"]
   
- }
-resource "aws_vpc" "vpc_v" {
-  cidr_block       = "10.1.0.0/16"
-  instance_tenancy = "default"
+#  }
+# resource "aws_vpc" "vpc_v" {
+#   cidr_block       = "10.1.0.0/16"
+#   instance_tenancy = "default"
 
-  tags = {
-    Name = "vpc_my"
-  }
-}
+#   tags = {
+#     Name = "vpc_my"
+#   }
+# }

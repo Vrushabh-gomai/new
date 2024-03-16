808fbdfe367149f347c81e4cd75da73cddbf2aa5
1 <1@gmail.com>
2024-03-16 09:09:58 +0530

    dd

--- Sg_secblock.tf
+++ Sg_secblock.tf
@@ -19,16 +19,30 @@ terraform {
   }
   variable "region" {
   type = string
-  description = "here i am providing region for my script"
+  description = " region for my script"
   default = "ap-southeast-1"
 }
 variable "ami" {
   type = string
-  description = "here we are providing ami for my project"
+  description = " ami for my project"
   default = "ami-09b1e8fc6368b8a3a"
 }
 variable "instance_type" {
   type = string
-  description = "here i am providing the server size"
+  description = " server size"
   default = "t2.micro"
+}
+variable "key_name" {
+  type = string
+  description = " key-pair of my server"
+  default = "key"
+}
+variable "tags" {
+  type = map
+  description = " tags to my server"
+  default = {
+    Name = "variable-server"
+    Enviorment = "production"
+    Project = "kit-kat Project"
+  }
 }
--- s3_var.tf
+++ s3_var.tf
@@ -1,40 +1,40 @@
- terraform {
-    required_providers {
+#  terraform {
+#     required_providers {
     
-    }
-  }
+#     }
+#   }
 
-  provider "aws" {
-      region = "ap-southeast-1"
-      shared_config_files = [ "/root/.aws/config" ]
-      shared_credentials_files = [ "/root/.aws/credentials"]
+#   provider "aws" {
+#       region = "ap-southeast-1"
+#       shared_config_files = [ "/root/.aws/config" ]
+#       shared_credentials_files = [ "/root/.aws/credentials"]
   
- }
-  resource "aws_s3_bucket" "walki" {
-  bucket = var.bucket
+#  }
+#   resource "aws_s3_bucket" "walki" {
+#   bucket = var.bucket
 
-  tags = var.tags
-  }
-  variable "region" {
-    type = string
-    description = "region"
-    default = "ap-southeast-1"
-  }
+#   tags = var.tags
+#   }
+#   variable "region" {
+#     type = string
+#     description = "region"
+#     default = "ap-southeast-1"
+#   }
 
 
 
- variable "bucket" {
-    type = string
-    description = "bucket"
-    default = "walki"
-  }
+#  variable "bucket" {
+#     type = string
+#     description = "bucket"
+#     default = "walki"
+#   }
 
- variable "tags" {
-    type = map
-    description = "region"
-    default = {
-        name = "walki"
+#  variable "tags" {
+#     type = map
+#     description = "region"
+#     default = {
+#         name = "walki"
 
-     }
-   }
+#      }
+#    }
 

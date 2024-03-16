504273fc70f1b1a05382c1df2ea90ea85ce51471
1 <1@gmail.com>
2024-03-16 21:08:49 +0530

    dd

--- standard_ec2.tf/main.tf
+++ standard_ec2.tf/main.tf
@@ -1,34 +1,18 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 3.0"
-    }
-  }
-
-
-  backend "s3" {
+provider "aws" {
+    region = "ap-southeast-1"
+    shared_config_files = [ "/root/.aws/config" ]
+    shared_credentials_files = [ "/root/.aws/credentials"]
+}
+    terraform {
+      backend "s3" {
     bucket         = "terrabucket1.1"
     key            = "terraform.tfstate"
     region         = "ap-southeast-1"
     encrypt        = true
     dynamodb_table = "terraform_b20"
-  }
-}
-provider "aws" {
-    region = "ap-southeast-1"
-    shared_config_files = [ "/root/.aws/config" ]
-    shared_credentials_files = [ "/root/.aws/credentials"]
-  
-}
-
-provider "aws" {
-    region = var.region
-    shared_config_files = [ "/root/.aws/config" ]
-    shared_credentials_files = [ "/root/.aws/credentials"]
-    
-}
-
+        
+      }
+    }
 resource "aws_instance" "my_instance_5" {
   ami = var.ami
   instance_type = var.instance_type
